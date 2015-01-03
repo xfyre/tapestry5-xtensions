3afcb86838983caf227cb82f7da132f0caea5599
Ilya Obshadko <xfyre@xfyre.com>
2015-01-03 22:19:20 +0200

    Add support for dynamic loading in popover using AJAX

--- src/main/java/com/xfyre/tapestry5/xtensions/mixins/BsPopover.java
+++ src/main/java/com/xfyre/tapestry5/xtensions/mixins/BsPopover.java
@@ -1,25 +1,51 @@
 package com.xfyre.tapestry5.xtensions.mixins;
 
-import org.apache.tapestry5.ClientElement;
-import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.*;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.InjectContainer;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONObject;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 /**
- * Bootstrap popover component support
+ * Bootstrap popover component support. Allows dynamic content loading.
+ * 
  * @author xfire
  *
  */
 public class BsPopover {
+    /**
+     * If this parameter is specified, specified Tapestry event will be triggered
+     * using AJAX. Make sure to provide appropriate event handler. Event handler
+     * is supposed to return JSONObject("content", "your_popover_content")
+     */
+    @Parameter(required = false, defaultPrefix = BindingConstants.LITERAL)
+    private String             popoverEvent;
+
+    @Parameter(required = false, defaultPrefix = BindingConstants.LITERAL)
+    private Object[]           popoverContext;
+
     @InjectContainer
-    private ClientElement ownerElement;
-    
+    private ClientElement      ownerElement;
+
     @Environmental
-    private JavaScriptSupport jsSupport;
-    
+    private JavaScriptSupport  jsSupport;
+
+    @Inject
+    private ComponentResources resources;
+
     void afterRender ( MarkupWriter writer ) {
-        jsSupport.require ( "t5xtensions/bspopover" ).with ( ownerElement.getClientId () );
+        if ( popoverEvent == null ) {
+            JSONObject params = new JSONObject ( "clientId", ownerElement.getClientId () );
+            jsSupport.require ( "t5xtensions/bspopover" ).with ( params );
+        } else {
+            Link eventLink = resources.createEventLink ( popoverEvent, popoverContext );
+            JSONObject params = new JSONObject ( 
+                    "clientId", ownerElement.getClientId (), 
+                    "eventLink", eventLink.toAbsoluteURI () );
+            jsSupport.require ( "t5xtensions/bspopover" ).with ( params );
+        }
     }
 
 }
--- src/main/resources/META-INF/modules/t5xtensions/bspopover.js
+++ src/main/resources/META-INF/modules/t5xtensions/bspopover.js
@@ -1,7 +1,21 @@
 define(["jquery", "t5/core/events", "bootstrap/popover"], function($, events) {
-    return function(clientId) {
+    return function(params) {
         var setupPopover = function(){
-            $('#' + clientId).popover({'container':'body'});
+            if (params.eventLink) {
+                $('#' + params.clientId).popover({
+                    'container' : 'body',
+                    'content'   : function() {
+                        $.ajax({
+                            'url'     : params.eventLink, 
+                            'success' : function(response) {
+                                $('.popover-content').html(response.content);
+                            }
+                        });
+                    }
+                });
+            } else {
+                $('#' + params.clientId).popover({'container':'body'});
+            }
         };
         
         $(document.body).on(events.zone.update, setupPopover);
