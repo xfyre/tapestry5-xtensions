af3b67db11b6164772413e15944cd0f05a411bfe
Ilya Obshadko <xfyre@xfyre.com>
2015-01-26 11:24:47 +0200

    fix confirmation dialog stale events

--- src/main/resources/META-INF/modules/t5xtensions/bsconfirm.js
+++ src/main/resources/META-INF/modules/t5xtensions/bsconfirm.js
@@ -10,27 +10,6 @@ define(["jquery", "t5/core/events", "t5/core/dom", "bootstrap/modal"], function(
         var modalDialogId   = "bootstrap_confirmation_modal_" + spec.messagehash;
         var confirmButtonId = "bootstrap_confirmation_button_" + spec.messagehash;
         
-        if (!$('#' + modalDialogId).length)
-            $(document.body).append (
-                '<div class="modal fade" role="dialog" id="' + modalDialogId + '">' +
-                    '<div class="modal-dialog">' +
-                        '<div class="modal-content">' +
-                            '<div class="modal-header">' +
-                                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                                '<h3 class="modal-title">' + title + '</h3>' +
-                            '</div>' +
-                            '<div class="modal-body">' +
-                                '<p>' + message + '</p>' +
-                            '</div>' +
-                            '<div class="modal-footer">' +
-                                '<button class="btn ' + spec.confirmClass + '" id="' + confirmButtonId + '">' + confirm + '</button>' +
-                                '<button class="btn" data-dismiss="modal">' + cancel + '</button>' + 
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                '</div>'                
-            );
-
         $('#' + clientId).data('confirmationPassed', false);
         $('#' + clientId).bind('click', function(event){
             if ($('#' + clientId).data('confirmationPassed')) {
@@ -42,7 +21,29 @@ define(["jquery", "t5/core/events", "t5/core/dom", "bootstrap/modal"], function(
                 event.stopImmediatePropagation();
                 console.log('confirmation on ' + clientId + ' requested');
 
+                if (!$('#' + modalDialogId).length)
+                    $(document.body).append (
+                        '<div class="modal fade" role="dialog" id="' + modalDialogId + '">' +
+                            '<div class="modal-dialog">' +
+                                '<div class="modal-content">' +
+                                    '<div class="modal-header">' +
+                                        '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                                        '<h3 class="modal-title">' + title + '</h3>' +
+                                    '</div>' +
+                                    '<div class="modal-body">' +
+                                        '<p>' + message + '</p>' +
+                                    '</div>' +
+                                    '<div class="modal-footer">' +
+                                        '<button class="btn ' + spec.confirmClass + '" id="' + confirmButtonId + '">' + confirm + '</button>' +
+                                        '<button class="btn" data-dismiss="modal">' + cancel + '</button>' + 
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>'                
+                    );
+                
                 $('#' + modalDialogId).modal({keyboard:true});
+                $('#' + modalDialogId).on('hidden.bs.modal', function(){ $('#' + modalDialogId).remove() });
                 $('#' + confirmButtonId).bind('click', function(confirmEvent){
                     $('#' + clientId).data('confirmationPassed', true);
                     $('#' + modalDialogId).modal('hide');
