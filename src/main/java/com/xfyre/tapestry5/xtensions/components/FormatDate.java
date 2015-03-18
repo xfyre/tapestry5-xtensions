8ec0da4435d0b6df341830cfd710700b24b6181c
Ilya Obshadko <xfyre@xfyre.com>
2015-03-18 21:07:42 +0200

    Allow persistent locale-based formatting for DateFormat

--- src/main/java/com/xfyre/tapestry5/xtensions/components/FormatDate.java
+++ src/main/java/com/xfyre/tapestry5/xtensions/components/FormatDate.java
@@ -7,6 +7,8 @@ import java.util.Date;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PersistentLocale;
 
 /**
  * Convenience component for formatting dates
@@ -19,18 +21,22 @@ public class FormatDate {
      */
     @Parameter(required=true,defaultPrefix=BindingConstants.PROP)
     private Date date;
-    
+
     /**
      * Format string
      */
-    @Parameter(required=true,defaultPrefix=BindingConstants.LITERAL)
+    @Parameter(required=false,defaultPrefix=BindingConstants.LITERAL)
     private String format;
-    
+
+    @Inject
+    private PersistentLocale persistentLocale;
+
     void beginRender ( MarkupWriter writer ) {
         if ( date == null ) {
             writer.writeRaw ( "" );
         } else {
-            DateFormat df = new SimpleDateFormat ( format );
+            DateFormat df = format == null ?
+                    DateFormat.getDateInstance ( DateFormat.SHORT, persistentLocale.get () ) : new SimpleDateFormat ( format );
             writer.writeRaw ( df.format ( date ) );
         }
     }
