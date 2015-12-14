5865e31f5f3c39ca3f437f75335a6cc608d39759
Ilya Obshadko <xfyre@xfyre.com>
2015-12-14 19:10:36 +0200

    Minor fixes

--- src/main/java/com/xfyre/tapestry5/xtensions/mixins/BsSelect.java
+++ src/main/java/com/xfyre/tapestry5/xtensions/mixins/BsSelect.java
@@ -5,6 +5,7 @@ import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.InjectContainer;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 public class BsSelect {
@@ -16,7 +17,7 @@ public class BsSelect {
 
     @Import(stylesheet="selectpicker/bootstrap-select.min.css",library="selectpicker/bootstrap-select.min.js")
     void afterRender ( MarkupWriter writer ) {
-        jsSupport.require ( "t5xtensions/bsselect" ).with ( ownerElement.getClientId () );
+        jsSupport.require ( "t5xtensions/bsselect" ).priority ( InitializationPriority.LATE ).with ( ownerElement.getClientId () );
     }
 
 }
--- src/main/resources/META-INF/modules/t5xtensions/bsselect.js
+++ src/main/resources/META-INF/modules/t5xtensions/bsselect.js
@@ -1,9 +1,3 @@
 define(["jquery", "t5/core/events"], function($, events) {
-    return function(clientId) {
-        var setupSelect = function() {
-            $('#' + clientId).selectpicker()
-        }
-        $(document.body).on(events.zone.update, setupSelect)
-        setupSelect()
-    };
-});
+    return function(clientId) { $('#' + clientId).selectpicker() }
+})
